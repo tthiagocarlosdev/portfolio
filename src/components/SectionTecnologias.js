d602534e9083704d787d64d39c0fbbd468866944
tthiagocarlosdev <tthiagocarlos.dev@gmail.com>
2024-04-09 23:02:25 -0300

    portifolio v1.0 - correcao da className nos icons da sectionTecnologias

--- src/components/SectionTecnologias.js
+++ src/components/SectionTecnologias.js
@@ -13,12 +13,12 @@ const SectionTecnologias = () => {
                 <LinhaComSeta/>
                 <div className="containerCirculosDeTecnologias">
                     <div className="circulosDeTecnologias"> <i className="fa-brands fa-html5"></i> </div>
-                    <div className="circulosDeTecnologias"> <i class="fa-brands fa-css3-alt"></i> </div>
-                    <div className="circulosDeTecnologias"> <i class="fa-brands fa-js"></i> </div>
-                    <div className="circulosDeTecnologias"> <i class="fa-brands fa-react"></i> </div>
-                    <div className="circulosDeTecnologias"> <i class="fa-brands fa-git-alt"></i> </div>
-                    <div className="circulosDeTecnologias"> <i class="fa-brands fa-github"></i> </div>
-                    <div className="circulosDeTecnologias"> <i class="fa-brands fa-node-js"></i> </div>
+                    <div className="circulosDeTecnologias"> <i className="fa-brands fa-css3-alt"></i> </div>
+                    <div className="circulosDeTecnologias"> <i className="fa-brands fa-js"></i> </div>
+                    <div className="circulosDeTecnologias"> <i className="fa-brands fa-react"></i> </div>
+                    <div className="circulosDeTecnologias"> <i className="fa-brands fa-git-alt"></i> </div>
+                    <div className="circulosDeTecnologias"> <i className="fa-brands fa-github"></i> </div>
+                    <div className="circulosDeTecnologias"> <i className="fa-brands fa-node-js"></i> </div>
                 </div>
             </div>
         </section>
