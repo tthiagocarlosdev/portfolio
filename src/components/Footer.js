8f09820691d8f6067ca3ed77476670db10e659e5
tthiagocarlosdev <tthiagocarlos.dev@gmail.com>
2024-04-12 16:35:05 -0300

    portifolio v1.6 - estilizacao responsiva do componente Footer finalizada.

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,10 +4,10 @@ import "./Footer.css"
 const Footer = () => {
 
     return ( 
-        <footer className="rodape">
-            <div className="larguraDaPagina">
-                <p className="textoRodape">Desenvolvido por <a href="#cabecalho"> tthiagocarlosdev</a> </p>
-                <a className="setaRodape" href="#cabecalho"> <i className="fa-solid fa-circle-arrow-up"></i></a>
+        <footer className="rodape displayFlexCenterCenter">
+            <div className="containerLarguraDoRodape displayFlexCenterCenter larguraDaPagina">
+                <p className="textoRodape displayFlexCenterCenter">Desenvolvido por <a href="#cabecalho"> tthiagocarlosdev</a> </p>
+                <a className="setaRodape displayFlexCenterCenter" href="#cabecalho"> <i className="fa-solid fa-circle-arrow-up"></i></a>
             </div>
         </footer>
      );
--- src/components/SectionTecnologias.js
+++ src/components/SectionTecnologias.js
@@ -1,5 +1,4 @@
 import React from "react";
-import Teste from "./Teste";
 import "./SectionTecnologias.css"
 
 import SubTituloDaSecao from './SubTituloDaSecao';
