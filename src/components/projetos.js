81205e37f4b1b8a084c60aa979d0ab305246d1b4
tthiagocarlosdev <tthiagocarlos.dev@gmail.com>
2024-04-12 17:40:42 -0300

    portifolio v1.7 - adicionado imagens, texto e endereco url nos cards dos projetos hora do dia, gerador de senha, snake case, password generator, jankenpo e my projects

--- src/components/projetos.js
+++ src/components/projetos.js
@@ -1,35 +1,39 @@
 const projetos = [
-    {
-      imagem: 'myProjects.png',
-      texto: 'Concentrador de links com alguns dos meus projetos desenvolvidos apenas com HTML, CSS e JavaScript puro.',
-      enderecoProjeto: 'https://tthiagocarlosdev.github.io/projects_website/'
-    },
-    {
-      imagem: 'myProjects.png',
-      texto: 'Concentrador de links com alguns dos meus projetos desenvolvidos apenas com HTML, CSS e JavaScript puro.',
-      enderecoProjeto: 'https://tthiagocarlosdev.github.io/projects_website/'
-    },
-    {
-      imagem: 'myProjects.png',
-      texto: 'Concentrador de links com alguns dos meus projetos desenvolvidos apenas com HTML, CSS e JavaScript puro.',
-      enderecoProjeto: 'https://tthiagocarlosdev.github.io/projects_website/'
-    },
-    {
-      imagem: 'myProjects.png',
-      texto: 'Concentrador de links com alguns dos meus projetos desenvolvidos apenas com HTML, CSS e JavaScript puro.',
-      enderecoProjeto: 'https://tthiagocarlosdev.github.io/projects_website/'
-    },
-    {
-      imagem: 'myProjects.png',
-      texto: 'Concentrador de links com alguns dos meus projetos desenvolvidos apenas com HTML, CSS e JavaScript puro.',
-      enderecoProjeto: 'https://tthiagocarlosdev.github.io/projects_website/'
-    },
     // {
     //   imagem: '',
     //   texto: '',
     //   enderecoProjeto: ''
     // },
-    
+    {
+      imagem: 'myProjects.png',
+      texto: 'Concentrador de links com alguns dos meus projetos desenvolvidos apenas com HTML, CSS e JavaScript puro.',
+      enderecoProjeto: 'https://tthiagocarlosdev.github.io/projects_website/'
+    },
+    {
+      imagem: 'jankenpon.png',
+      texto: 'Jogo pedra, papel, tesoura! Também conhecido como jokenpô. Jogue contra o computadore vença se for possível!',
+      enderecoProjeto: 'https://tthiagocarlosdev.github.io/projects_website/projetos/jankenpon/index.html'
+    },
+    {
+      imagem: 'passwordGenerator.png',
+      texto: '2ª versão do site gerador de senha segura! Com botão para o usuário copiar a sua senha para a área de transferência.',
+      enderecoProjeto: 'https://tthiagocarlosdev.github.io/projects_website/projetos/password-generator/index.html'
+    },
+    {
+      imagem: 'snakeCase.png',
+      texto: 'Recebe um texto qualquer e retorna uma string no formato snake_case. Também informa a quantidade de caracteres da string.',
+      enderecoProjeto: 'https://tthiagocarlosdev.github.io/projects_website/projetos/snake_case/index.html'
+    },
+    {
+      imagem: 'geradorDeSenha.png',
+      texto: '1ª versão do site gerador de senha segura! Senha com letras minúsculas, maiúsculas, números e símbolos!',
+      enderecoProjeto: 'https://tthiagocarlosdev.github.io/projects_website/projetos/gerador_de_senha/index.html'
+    },
+    {
+      imagem: 'horaDoDia.png',
+      texto: 'Este foi um dos meus primeiros projetos! Este site muda seu layout de acordo com a hora do dia!',
+      enderecoProjeto: 'https://tthiagocarlosdev.github.io/projects_website/projetos/horas_do_dia/horasDoDia.html'
+    },
 ];
 
 export default projetos;
